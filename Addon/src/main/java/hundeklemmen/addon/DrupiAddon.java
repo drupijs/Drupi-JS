076f2e1d6e59ae718bf93ec9842528e7ed18a215
Alexander <Thehundeklemmen@gmail.com>
2019-05-15 22:24:40 +0200

    working on drupi addon api

---
+++ Addon/src/main/java/hundeklemmen/addon/DrupiAddon.java
@@ -0,0 +1,15 @@
+package hundeklemmen.addon;
+
+public class DrupiAddon {
+
+    private String name;
+
+    public DrupiAddon(String name){
+        this.name = name;
+    }
+
+
+    public String getName(){
+        return this.name;
+    }
+}
