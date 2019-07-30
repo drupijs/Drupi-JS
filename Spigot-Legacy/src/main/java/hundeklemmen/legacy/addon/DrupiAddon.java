cc31575ade7aa74594f6afd02d5852a3c88b7337
Alexander <Thehundeklemmen@gmail.com>
2019-07-30 21:16:56 +0200

    Addon api

---
+++ Spigot-Legacy/src/main/java/hundeklemmen/legacy/addon/DrupiAddon.java
@@ -0,0 +1,28 @@
+package hundeklemmen.legacy.addon;
+
+import hundeklemmen.legacy.MainPlugin;
+import hundeklemmen.shared.api.Drupi;
+
+public class DrupiAddon {
+
+    private String name;
+    private DrupiAddon instance;
+    private Drupi drupi;
+
+    public DrupiAddon(String name, Object clas){
+        this.name = name;
+        this.instance = this;
+        this.drupi =  MainPlugin.drupi;
+        drupi.registerManager(name, clas);
+        drupi.log.info("Registered Addon: " + name);
+    }
+
+    public void callEvent(String name, Object... event){
+        drupi.callFunction(this.name + "_" + name, event);
+    }
+    public Object callEventWithResult(String name, Object... event){
+        return drupi.callFunctionWithResult(this.name + "_" + name, event);
+    }
+
+
+}
